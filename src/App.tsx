5e60b9e7496782f8f1700338ce21a172d9310fd4
Diego <diego.martini@alumnos.upm.es>
2025-11-10 22:10:37 +0100

    ola

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,10 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
-import NotFound from "./pages/NotFound";
-import FullMenu from "./pages/FullMenu"; // NUEVA IMPORTACIÓN
+// IMPORTACIONES CORREGIDAS CON ALIAS
+import Index from "@/pages/Index"; 
+import NotFound from "@/pages/NotFound";
+import FullMenu from "@/pages/FullMenu"; 
 
 const queryClient = new QueryClient();
 
@@ -17,7 +18,7 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="/menu-completo" element={<FullMenu />} /> {/* NUEVA RUTA */}
+          <Route path="/menu-completo" element={<FullMenu />} /> 
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
